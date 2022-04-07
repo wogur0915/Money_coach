b830f6f7cf831f2ac4525dce39c17b1d807afeb7
Andrewendlesss <andrewendlesss@gmail.com>
2022-04-07 15:19:19 +0900

    Added the main window, which only has a title, myClick function, frame function, and the positioning x and y

---
+++ 가계부.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+root = Tk()
+root.title("House-hold account helper")
+root.iconbitmap('c:/...')
+def myClick():
+    return
+
+frame = LabelFrame(root, text = "", padx = 35, pady=35)
+frame.grid(padx=10, pady=10)
+root.mainloop() 
