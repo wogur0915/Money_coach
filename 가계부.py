1558e47a3db44fdf4979078d340345b4a7e42bf3
Andrewendlesss <andrewendlesss@gmail.com>
2022-04-08 11:17:17 +0900

    Added 3 buttons for 수입,지출,통계

--- 가계부.py
+++ 가계부.py
@@ -8,4 +8,16 @@ def myClick():
 
 frame = LabelFrame(root, text = "", padx = 35, pady=35)
 frame.grid(padx=10, pady=10)
+
+myButton1 = Button(frame, text="수입",padx = 16, pady = 10,  command = myClick)
+myButton1.grid(row = 0, column = 0)
+
+myButton2 = Button(frame, text="지출", padx = 16, pady = 10, command = myClick)
+myButton2.grid(row = 0, column = 1)
+
+myButton3 = Button(frame, text="통계",padx = 16, pady = 10,  command = myClick)
+myButton3.grid(row = 0, column = 2)
+myButton4 = Button(frame, text="예비",padx = 16, pady = 10, command = myClick)
+myButton4.grid(row = 0, column = 3)
+
 root.mainloop() 
