d13018add8a199d9b058953ebc683f41a3919dae
Andrewendlesss <andrewendlesss@gmail.com>
2022-04-08 11:27:00 +0900

    Added 3 more buttons, and an exit button, this window is still incomplete

--- 가계부.py
+++ 가계부.py
@@ -17,7 +17,17 @@ myButton2.grid(row = 0, column = 1)
 
 myButton3 = Button(frame, text="통계",padx = 16, pady = 10,  command = myClick)
 myButton3.grid(row = 0, column = 2)
+
 myButton4 = Button(frame, text="예비",padx = 16, pady = 10, command = myClick)
 myButton4.grid(row = 0, column = 3)
+
+myButton6 = Button(frame, text="예산", padx = 16, pady=10, command = myClick)
+myButton6.grid(row = 0, column = 4)
+
+myButton5 = Button(root, text="  사진  ", padx = 230, pady=80, fg = "red")
+myButton5.grid(row = 3, column = 0)
+
+button_quit = Button(root, text="Exit Program", command=root.quit, padx=50, pady = 0)
+button_quit.grid(padx=0, pady = 0)
 
 root.mainloop() 
