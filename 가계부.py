a69581522508c4fdb1790db9369208c2f6329857
Basilion <47267113+KorBasilion@users.noreply.github.com>
2022-04-14 01:06:52 +0900

    [Feat] 메인화면 프레임 작업
    기존 메인화면 코드의 전체적인 리메이킹 작업

--- 가계부.py
+++ 가계부.py
@@ -1,20 +1,43 @@
 from tkinter import *
 
-root = Tk()
-root.title("House-hold account helper")
-root.iconbitmap('c:/...')
-def myClick():
-    return
-Label(root, text= "", font= ('H')).grid(row=0, column=3, padx= 40, pady= 85)
+# Default Windows Setting
+main = Tk()
+main.title("House-hold Account Book System")
+main.resizable(width = False, height = False)
+main.geometry('832x500')
+#main.iconbitmap('c:/...')
 
-myButton1 = Button(root, text="내역",padx = 16, pady = 10,  command = myClick)
-myButton1.grid(row = 10, column = 0)
+# Default Frame Setting
+lobbyFrame = Frame(main)
+historyFrame = Frame(main)
+statisticsFrame = Frame(main)
+assetsFrame = Frame(main)
+settingFrame = Frame(main)
 
-myButton2 = Button(root, text="통계", padx = 16, pady = 10, command = myClick)
-myButton2.grid(row = 10, column = 1)
+lobbyFrame.grid(row=0, column=0, sticky="nsew")
+historyFrame.grid(row=0, column=0, sticky="nsew")
+statisticsFrame.grid(row=0, column=0, sticky="nsew")
+assetsFrame.grid(row=0, column=0, sticky="nsew")
+settingFrame.grid(row=0, column=0, sticky="nsew")
 
-myButton3 = Button(root, text="자산",padx = 16, pady = 10,  command = myClick)
-myButton3.grid(row = 10, column = 2)
-myButton4 = Button(root, text="설정",padx = 16, pady = 10, command = myClick)
-myButton4.grid(row = 10, column = 3)
-root.mainloop() 
+def show_frame(frame):
+    frame.tkraise()
+
+test=PhotoImage(file='1pixel.png')
+
+lobbyLogo = Label(lobbyFrame, image=test, text="Logo", width=800, height=446, compound="c")
+lobbyLogo.grid(row=1, column=0, columnspan=4)
+
+for frameName in (lobbyFrame, historyFrame, statisticsFrame, assetsFrame, settingFrame):
+    historyButton = Button(frameName, image=test, text="내역", width=200, height=40, compound="c", command=lambda:[show_frame(historyFrame)])
+    historyButton.grid(row=0, column=0)
+    statisticsButton = Button(frameName, image=test, text="통계", width=200, height=40, compound="c", command=lambda:[show_frame(statisticsFrame)])
+    statisticsButton.grid(row=0, column=1)
+    assetsButton = Button(frameName, image=test, text="자산", width=200, height=40, compound="c", command=lambda:[show_frame(assetsFrame)])
+    assetsButton.grid(row=0, column=2)
+    settingButton = Button(frameName, image=test, text="설정", width=200, height=40, compound="c", command=lambda:[show_frame(settingFrame)])
+    settingButton.grid(row=0, column=3)
+
+# Main Roop & Set First Frame
+show_frame(lobbyFrame)
+main.mainloop()
