300fd44187bfca67e72ea7a6533e6623b3c4afcf
wogur0915 <34836246+wogur0915@users.noreply.github.com>
2022-04-23 09:58:47 +0900

    수입, 지출 진입 버튼 생성 및 이벤트 코드 수정
    수입 버튼만 있던 것에서 지출 버튼 추가, 버튼 클릭 시 열리는 창에서 입력한 정보가 정확하게 전달되는지를 파악하기 위해 터미널에 출력 지시.
    같은 코드를 지출창에도 적용

--- historyframe.py
+++ historyframe.py
@@ -5,6 +5,12 @@ root.title("수입 / 지출")
 root.geometry("300x300")
 
 def incomeWindow() :
+    def incomeInputVal() :
+        getDate = inputDate.get()
+        getIncome = inputIncome.get()
+        getType = incomeType.get()
+        print(getDate, getIncome, getType)
+
     types = ['식비','주거/통신','의복/미용','건강/문화','교육/육아','교통/차량','경조사/회비','공과금','월급','기타']
     incomeWindow = Tk()
     incomeWindow.title("수입")
@@ -21,13 +27,41 @@ def incomeWindow() :
     inputIncome = Entry(incomeWindow)
     inputIncome.grid(row=4, column=1)
     incomeType.grid(row=4, column=2)
-    incomeInputBtn = Button(incomeWindow, text = "확인")
+    incomeInputBtn = Button(incomeWindow, text = "확인", command = incomeInputVal)
     incomeInputBtn.grid(row=5, column=1)
+
+
+def expenseWindow() :
+    def expenseInputVal() :
+        getDate = inputDate.get()
+        getIncome = inputExpense.get()
+        getType = expenseType.get()
+        print(getDate, getIncome, getType)
+
+    types = ['식비','주거/통신','의복/미용','건강/문화','교육/육아','교통/차량','경조사/회비','공과금','월급','기타']
+    expenseWindow = Tk()
+    expenseWindow.title("지출")
+    expenseWindow.geometry("500x250")
+    expenseLb1 = Label(expenseWindow, text="날짜", font="나눔고딕 15")
+    expenseLb2 = Label(expenseWindow, text="금액", font="나눔고딕 15")
+    expenseLb3 = Label(expenseWindow, text="분류", font="나눔고딕 15")
+    expenseType = Combobox(expenseWindow, height=10, values=types)
+    expenseLb1.grid(row=3, column=0)
+    expenseLb2.grid(row=3, column=1)
+    expenseLb3.grid(row=3, column=2)
+    inputDate = Entry(expenseWindow)
+    inputDate.grid(row=4, column=0)
+    inputExpense = Entry(expenseWindow)
+    inputExpense.grid(row=4, column=1)
+    expenseType.grid(row=4, column=2)
+    expenseInputBtn = Button(expenseWindow, text = "확인", command = expenseInputVal)
+    expenseInputBtn.grid(row=5, column=1)
 
 incomeBtn = Button(root, text = "수입", font="나눔고딕 10", command = incomeWindow)
 incomeBtn.config(width = 20, height = 5)
-
+expenseBtn = Button(root, text = "지출", font="나눔고딕 10", command = expenseWindow)
+expenseBtn.config(width = 20, height = 5)
 incomeBtn.pack()
-
+expenseBtn.pack()
 
 root.mainloop() #위에서 생성한 객체.mainloop
