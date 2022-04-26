06ce9a812eed0633da07b94669f978b68547085d
wogur0915 <34836246+wogur0915@users.noreply.github.com>
2022-04-26 11:39:31 +0900

    첫 화면 버튼 수정 수정 및 가계부 내역 입력 창 구성 변경
    첫 화면에서의 버튼 구성을 내역출력, 삭제, 추가 로 변경하였으며 내역출력을 통해 추가된 가계부 값들이 정상적으로 변수에 assignment 되는지를 확인하는 작업입니다.
    가계부 내역 입력창 또한 기존에 가로로 배열되어있던 것을 편의에 맞게 새로에 맞게 수정하였으며 비고란을 추가하였습니다.
    기존에있던 수입/지출 을 나눠놓은 버튼은 삭제하였습니다.

--- historyframe.py
+++ historyframe.py
@@ -1,67 +1,73 @@
 from tkinter import *
 from tkinter.ttk import Combobox
+
 root = Tk()
-root.title("수입 / 지출")
+root.title("내역")
 root.geometry("300x300")
 
+# DB 구성을 위한 Global List 변수 선언
+dates = []
+money = []
+types = []
+otherDetails =[]
+
+# 가계부 내역 추가 버튼
 def incomeWindow() :
+
+    # 입력받은 데이터를 Global 변수로 리턴하여 내역에 적용
     def incomeInputVal() :
+        global dates, money, types, otherDetails
         getDate = inputDate.get()
         getIncome = inputIncome.get()
+        getInputOthers = inputOthers.get()
         getType = incomeType.get()
-        print(getDate, getIncome, getType)
 
-    types = ['식비','주거/통신','의복/미용','건강/문화','교육/육아','교통/차량','경조사/회비','공과금','월급','기타']
+        dates.append(getDate)
+        money.append(getIncome)
+        types.append(getType)
+        otherDetails.append(getInputOthers)
+
     incomeWindow = Tk()
     incomeWindow.title("수입")
-    incomeWindow.geometry("500x250")
+    incomeWindow.geometry("250x250")
+
+    # 입력창
     incomeLb1 = Label(incomeWindow, text="날짜", font="나눔고딕 15")
     incomeLb2 = Label(incomeWindow, text="금액", font="나눔고딕 15")
-    incomeLb3 = Label(incomeWindow, text="분류", font="나눔고딕 15")
-    incomeType = Combobox(incomeWindow, height=10, values=types)
-    incomeLb1.grid(row=3, column=0)
-    incomeLb2.grid(row=3, column=1)
-    incomeLb3.grid(row=3, column=2)
+    incomeLb3 = Label(incomeWindow, text="카테고리", font="나눔고딕 15")
+    incomeLb4 = Label(incomeWindow, text="비고", font="나눔고딕 15")
+    incomeLb1.grid(row=0, column=1)
+    incomeLb2.grid(row=2, column=1)
+    incomeLb3.grid(row=4, column=1)
+    incomeLb4.grid(row=6, column=1)
     inputDate = Entry(incomeWindow)
-    inputDate.grid(row=4, column=0)
     inputIncome = Entry(incomeWindow)
-    inputIncome.grid(row=4, column=1)
-    incomeType.grid(row=4, column=2)
-    incomeInputBtn = Button(incomeWindow, text = "확인", command = incomeInputVal)
-    incomeInputBtn.grid(row=5, column=1)
-
-
-def expenseWindow() :
-    def expenseInputVal() :
-        getDate = inputDate.get()
-        getIncome = inputExpense.get()
-        getType = expenseType.get()
-        print(getDate, getIncome, getType)
-
+    inputOthers = Entry(incomeWindow)
     types = ['식비','주거/통신','의복/미용','건강/문화','교육/육아','교통/차량','경조사/회비','공과금','월급','기타']
-    expenseWindow = Tk()
-    expenseWindow.title("지출")
-    expenseWindow.geometry("500x250")
-    expenseLb1 = Label(expenseWindow, text="날짜", font="나눔고딕 15")
-    expenseLb2 = Label(expenseWindow, text="금액", font="나눔고딕 15")
-    expenseLb3 = Label(expenseWindow, text="분류", font="나눔고딕 15")
-    expenseType = Combobox(expenseWindow, height=10, values=types)
-    expenseLb1.grid(row=3, column=0)
-    expenseLb2.grid(row=3, column=1)
-    expenseLb3.grid(row=3, column=2)
-    inputDate = Entry(expenseWindow)
-    inputDate.grid(row=4, column=0)
-    inputExpense = Entry(expenseWindow)
-    inputExpense.grid(row=4, column=1)
-    expenseType.grid(row=4, column=2)
-    expenseInputBtn = Button(expenseWindow, text = "확인", command = expenseInputVal)
-    expenseInputBtn.grid(row=5, column=1)
+    incomeType = Combobox(incomeWindow, height=10, values=types)
+    inputDate.grid(row=1, column=1)
+    inputIncome.grid(row=3, column=1)
+    incomeType.grid(row=5, column=1)
+    inputOthers.grid(row=7, column=1)
+    incomeInputBtn = Button(incomeWindow, text = "확인", command = incomeInputVal)
+    incomeInputBtn.grid(row=8, column=1)
 
-incomeBtn = Button(root, text = "수입", font="나눔고딕 10", command = incomeWindow)
-incomeBtn.config(width = 20, height = 5)
-expenseBtn = Button(root, text = "지출", font="나눔고딕 10", command = expenseWindow)
-expenseBtn.config(width = 20, height = 5)
-incomeBtn.pack()
-expenseBtn.pack()
+def test() :
+    print("=" * 10, "내역 출력", "="*10)
+    if len(dates) == 0 :
+        print("No data in Program")
+    else :
+        for i in range(0,len(dates)) :
+            print(dates[i], money[i], types[i], otherDetails[i])
 
-root.mainloop() #위에서 생성한 객체.mainloop
+incomeBtn = Button(root, text = "+", font="나눔고딕 10", command = incomeWindow)
+incomeBtn.config(width = 5, height = 5)
+deleteBtn = Button(root, text = "삭제", font="나눔고딕 10")
+deleteBtn.config(width = 5, height = 5)
+expenseBtn = Button(root, text = "내역출력", font="나눔고딕 10", command = test)
+expenseBtn.config(width = 5, height = 5)
+incomeBtn.pack(side='right')
+deleteBtn.pack(side='right')
+expenseBtn.pack(side='left')
+
+root.mainloop()
