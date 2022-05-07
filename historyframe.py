fa45a4ca81c15c6882bcf651b1c5e7f673db0583
JH_T0mmy <34836246+wogur0915@users.noreply.github.com>
2022-05-07 14:17:58 +0900

    프로그램 병합을 위해 첫화면 구성 변경 (TextArea 추가)
    버튼을 눌렀을 때 새로운 창이 나오는 것이 아닌 기존 프레임에 적용 시키기 위한 준비 작업.
    버튼으로만 구성된 화면을 TextArea 를 통해서 입력한 정보를 표시할 수 있게 임시 작업을 진행하였습니다.

--- historyframe.py
+++ historyframe.py
@@ -1,7 +1,9 @@
 from tkinter import *
+import tkinter
 from tkinter.ttk import Combobox
 
 root = Tk()
+textarea = Text(root)
 root.title("내역")
 root.geometry("300x300")
 
@@ -16,7 +18,7 @@ def incomeWindow() :
 
     # 입력받은 데이터를 Global 변수로 리턴하여 내역에 적용
     def incomeInputVal() :
-        global dates, money, types, otherDetails
+        global dates, money, types, otherDetails, i
         getDate = inputDate.get()
         getIncome = inputIncome.get()
         getInputOthers = inputOthers.get()
@@ -29,17 +31,17 @@ def incomeWindow() :
 
     incomeWindow = Tk()
     incomeWindow.title("수입")
-    incomeWindow.geometry("250x250")
+    incomeWindow.geometry("300x300")
 
     # 입력창
     incomeLb1 = Label(incomeWindow, text="날짜", font="나눔고딕 15")
     incomeLb2 = Label(incomeWindow, text="금액", font="나눔고딕 15")
     incomeLb3 = Label(incomeWindow, text="카테고리", font="나눔고딕 15")
     incomeLb4 = Label(incomeWindow, text="비고", font="나눔고딕 15")
-    incomeLb1.grid(row=0, column=1)
-    incomeLb2.grid(row=2, column=1)
-    incomeLb3.grid(row=4, column=1)
-    incomeLb4.grid(row=6, column=1)
+    incomeLb1.grid(row=0, column=1, padx=20, pady=5)
+    incomeLb2.grid(row=2, column=1, padx=20, pady=5)
+    incomeLb3.grid(row=4, column=1, padx=20, pady=5)
+    incomeLb4.grid(row=6, column=1, padx=20, pady=5)
     inputDate = Entry(incomeWindow)
     inputIncome = Entry(incomeWindow)
     inputOthers = Entry(incomeWindow)
@@ -60,12 +62,14 @@ def test() :
         for i in range(0,len(dates)) :
             print(dates[i], money[i], types[i], otherDetails[i])
 
+textarea.config(width = 250, height = 20)
 incomeBtn = Button(root, text = "+", font="나눔고딕 10", command = incomeWindow)
 incomeBtn.config(width = 5, height = 5)
 deleteBtn = Button(root, text = "삭제", font="나눔고딕 10")
 deleteBtn.config(width = 5, height = 5)
 expenseBtn = Button(root, text = "내역출력", font="나눔고딕 10", command = test)
 expenseBtn.config(width = 5, height = 5)
+textarea.pack(side='top')
 incomeBtn.pack(side='right')
 deleteBtn.pack(side='right')
 expenseBtn.pack(side='left')
