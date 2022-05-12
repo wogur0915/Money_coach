c65f9c3544478ebd296a7ce8ba5054f2ae35f7f2
JH_T0mmy <34836246+wogur0915@users.noreply.github.com>
2022-05-12 15:20:03 +0900

    Merge historyFrame to mainView
    메인 화면의 내역 부분 프레인에 기존에 따로 제작한 historyframe 을 얹어서 하나의 파일로 병합처리.
    (historyFrame.py는 추후 삭제예정, 백업용)

--- mainview.py
+++ mainview.py
@@ -1,4 +1,6 @@
 from tkinter import *
+import tkinter
+from tkinter.ttk import Combobox
 
 # Default Windows Setting
 main = Tk()
@@ -13,6 +15,7 @@ historyFrame = Frame(main)
 statisticsFrame = Frame(main)
 assetsFrame = Frame(main)
 settingFrame = Frame(main)
+textarea = Text(historyFrame)
 
 lobbyFrame.grid(row=0, column=0, sticky="nsew")
 historyFrame.grid(row=0, column=0, sticky="nsew")
@@ -37,6 +40,75 @@ for frameName in (lobbyFrame, historyFrame, statisticsFrame, assetsFrame, settin
     assetsButton.grid(row=0, column=2)
     settingButton = Button(frameName, image=test, text="설정", width=200, height=40, compound="c", command=lambda:[show_frame(settingFrame)])
     settingButton.grid(row=0, column=3)
+
+## 내역 부분
+
+# DB 구성을 위한 Global List 변수 선언
+dates = []
+money = []
+types = []
+otherDetails =[]
+i = 0
+
+# 가계부 내역 추가 버튼
+def incomeWindow() :
+
+    # 입력받은 데이터를 Global 변수로 리턴하여 내역에 적용
+    def incomeInputVal() :
+        global dates, money, types, otherDetails, i
+        getDate = inputDate.get()
+        getIncome = inputIncome.get()
+        getInputOthers = inputOthers.get()
+        getType = incomeType.get()
+
+        dates.append(getDate)
+        money.append(getIncome)
+        types.append(getType)
+        otherDetails.append(getInputOthers)
+        textarea.insert(END, dates[i], END, " "*20, END, money[i], END, " "*20, END, types[i], END, " "*20, END, otherDetails[i], END, "\n")
+        i = i+1
+
+        # 데이터 추가 후 Clean
+        inputDate.delete(0,END)
+        inputIncome.delete(0,END)
+        inputOthers.delete(0,END)
+        incomeType.delete(0,END)
+
+    incomeWindow = Tk()
+    incomeWindow.title("가계부 추가")
+    incomeWindow.geometry("300x300")
+
+   # 입력창
+    incomeLb1 = Label(incomeWindow, text="날짜", font="나눔고딕 15")
+    incomeLb2 = Label(incomeWindow, text="금액", font="나눔고딕 15")
+    incomeLb3 = Label(incomeWindow, text="카테고리", font="나눔고딕 15")
+    incomeLb4 = Label(incomeWindow, text="비고", font="나눔고딕 15")
+    incomeLb1.grid(row=0, column=1, padx=110, pady=7)
+    incomeLb2.grid(row=2, column=1, padx=110, pady=7)
+    incomeLb3.grid(row=4, column=1, padx=110, pady=7)
+    incomeLb4.grid(row=6, column=1, padx=110, pady=7)
+    inputDate = Entry(incomeWindow)
+    inputIncome = Entry(incomeWindow)
+    types = ['식비','주거/통신','의복/미용','건강/문화','교육/육아','교통/차량','경조사/회비','공과금','월급','기타']
+    incomeType = Combobox(incomeWindow, height=10, values=types)
+    inputOthers = Entry(incomeWindow)
+    inputDate.grid(row=1, column=1)
+    inputIncome.grid(row=3, column=1)
+    incomeType.grid(row=5, column=1)
+    inputOthers.grid(row=7, column=1)
+    incomeInputBtn = Button(incomeWindow, text = "확인", command = incomeInputVal)
+    incomeInputBtn.grid(row=8, column=1, padx=10, pady=10)
+
+
+textarea.config(width = 110, height = 30)
+textarea.insert(INSERT, "날짜", INSERT, " "*20, INSERT, "금액", INSERT, " "*20, INSERT, "카테고리", INSERT, " "*20, INSERT, "비고", INSERT, "\n")
+incomeBtn = Button(historyFrame, text = "+", font="나눔고딕 10", command = incomeWindow)
+incomeBtn.config(width = 5, height = 5)
+deleteBtn = Button(historyFrame, text = "삭제", font="나눔고딕 10")
+deleteBtn.config(width = 5, height = 5)
+textarea.grid(row=1, column=0, columnspan=4)
+deleteBtn.grid(row=2, column=2)
+incomeBtn.grid(row=2, column=3)
 
 # Main Roop & Set First Frame
 show_frame(lobbyFrame)
