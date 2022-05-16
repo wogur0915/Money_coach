0bd642a804f6e8469be25b9c09be98bf1f421c1f
JH_T0mmy <34836246+wogur0915@users.noreply.github.com>
2022-05-16 11:04:23 +0900

    Treeview 형식에 맞게 전체적인 코드 수정 #17
    Textarea를 Treeview 구조로 변경하였으며 전체적인 코드의 가독성을 높이고자 수정을 하였습니다.
    또한 내역 추가 창에 수입/지출 콤보 박스를 추가하여 수입과 지출 여부를 한 화면에서 나누어 입력 가능하게 구성하였습니다.

--- mainview.py
+++ mainview.py
@@ -15,7 +15,8 @@ historyFrame = Frame(main)
 statisticsFrame = Frame(main)
 assetsFrame = Frame(main)
 settingFrame = Frame(main)
-textarea = Text(historyFrame)
+
+treeview=tkinter.ttk.Treeview(historyFrame, columns=["one", "two", "three","four"])
 
 lobbyFrame.grid(row=0, column=0, sticky="nsew")
 historyFrame.grid(row=0, column=0, sticky="nsew")
@@ -47,66 +48,100 @@ for frameName in (lobbyFrame, historyFrame, statisticsFrame, assetsFrame, settin
 dates = []
 money = []
 types = []
-otherDetails =[]
+otherDetails = []
+expOrInc = []
+
+treelist=[]
 i = 0
 
 # 가계부 내역 추가 버튼
-def incomeWindow() :
+def addList() :
 
     # 입력받은 데이터를 Global 변수로 리턴하여 내역에 적용
     def incomeInputVal() :
         global dates, money, types, otherDetails, i
         getDate = inputDate.get()
-        getIncome = inputIncome.get()
+        getExpOrInc = inputExpOrInc.get()
+        getMoney = inputMoney.get()
         getInputOthers = inputOthers.get()
         getType = incomeType.get()
 
         dates.append(getDate)
-        money.append(getIncome)
+        expOrInc.append(getExpOrInc)
+        money.append(getMoney)
         types.append(getType)
         otherDetails.append(getInputOthers)
-        textarea.insert(END, dates[i], END, " "*20, END, money[i], END, " "*20, END, types[i], END, " "*20, END, otherDetails[i], END, "\n")
+
+        # textarea.insert(END, dates[i], END, " "*20, END, money[i], END, " "*20, END, types[i], END, " "*20, END, otherDetails[i], END, "\n")
+        treeview.insert('', 'end', text=dates[i], values=[expOrInc[i], money[i], types[i], otherDetails[i]], iid=str(i))
         i = i+1
 
         # 데이터 추가 후 Clean
         inputDate.delete(0,END)
-        inputIncome.delete(0,END)
+        inputExpOrInc.delete(0,END)
+        inputMoney.delete(0,END)
         inputOthers.delete(0,END)
         incomeType.delete(0,END)
 
     incomeWindow = Tk()
     incomeWindow.title("가계부 추가")
-    incomeWindow.geometry("300x300")
+    incomeWindow.geometry("300x350")
 
-   # 입력창
-    incomeLb1 = Label(incomeWindow, text="날짜", font="나눔고딕 15")
-    incomeLb2 = Label(incomeWindow, text="금액", font="나눔고딕 15")
-    incomeLb3 = Label(incomeWindow, text="카테고리", font="나눔고딕 15")
-    incomeLb4 = Label(incomeWindow, text="비고", font="나눔고딕 15")
-    incomeLb1.grid(row=0, column=1, padx=110, pady=7)
-    incomeLb2.grid(row=2, column=1, padx=110, pady=7)
-    incomeLb3.grid(row=4, column=1, padx=110, pady=7)
-    incomeLb4.grid(row=6, column=1, padx=110, pady=7)
-    inputDate = Entry(incomeWindow)
-    inputIncome = Entry(incomeWindow)
+    # 입력창
+    incomeLb1 = Label(incomeWindow, text="날짜", font="나눔고딕 13")
+    incomeLb2 = Label(incomeWindow, text="수입/지출", font="나눔고딕 13")
+    incomeLb3 = Label(incomeWindow, text="금액", font="나눔고딕 13")
+    incomeLb4 = Label(incomeWindow, text="카테고리", font="나눔고딕 13")
+    incomeLb5 = Label(incomeWindow, text="비고", font="나눔고딕 13")
+    incomeLb1.grid(row=0, column=1, padx=100, pady=7)
+    incomeLb2.grid(row=2, column=1, padx=100, pady=7)
+    incomeLb3.grid(row=4, column=1, padx=100, pady=7)
+    incomeLb4.grid(row=6, column=1, padx=100, pady=7)
+    incomeLb5.grid(row=8, column=1, padx=100, pady=7)
+
+    expOrIncTyp = ['수입', '지출']
     types = ['식비','주거/통신','의복/미용','건강/문화','교육/육아','교통/차량','경조사/회비','공과금','월급','기타']
-    incomeType = Combobox(incomeWindow, height=10, values=types)
-    inputOthers = Entry(incomeWindow)
-    inputDate.grid(row=1, column=1)
-    inputIncome.grid(row=3, column=1)
-    incomeType.grid(row=5, column=1)
-    inputOthers.grid(row=7, column=1)
+
+    inputDate = Entry(incomeWindow, justify = "center")
+    inputExpOrInc = Combobox(incomeWindow, width=17, height=10, values=expOrIncTyp, justify = "center")
+    inputMoney = Entry(incomeWindow, justify = "center")
+    incomeType = Combobox(incomeWindow, width=17, height=10, values=types, justify = "center")
+    inputOthers = Entry(incomeWindow, justify = "center")
     incomeInputBtn = Button(incomeWindow, text = "확인", command = incomeInputVal)
-    incomeInputBtn.grid(row=8, column=1, padx=10, pady=10)
+
+    inputDate.grid(row=1, column=1)
+    inputExpOrInc.grid(row=3, column=1)
+    inputMoney.grid(row=5, column=1)
+    incomeType.grid(row=7, column=1)
+    inputOthers.grid(row=9, column=1)
+    incomeInputBtn.grid(row=10, column=1, padx=10, pady=10)
+
+treeview.config(height = 18)
+treeview.column("#0", width=150, anchor="center")
+treeview.heading("#0", text="날짜", anchor="center")
+
+treeview.column("#1", width=100, anchor="center")
+treeview.heading("one", text="수입/지출", anchor="center")
+
+treeview.column("#2", width=100, anchor="center")
+treeview.heading("two", text="금액", anchor="center")
+
+treeview.column("#3", width=100, anchor="center")
+treeview.heading("three", text="카테고리", anchor="center")
+
+treeview.column("#4", width=300, anchor="center")
+treeview.heading("four", text="비고", anchor="center")
 
 
-textarea.config(width = 110, height = 30)
-textarea.insert(INSERT, "날짜", INSERT, " "*20, INSERT, "금액", INSERT, " "*20, INSERT, "카테고리", INSERT, " "*20, INSERT, "비고", INSERT, "\n")
-incomeBtn = Button(historyFrame, text = "+", font="나눔고딕 10", command = incomeWindow)
+# for i in range(len(treelist)):
+#     treeview.insert('', 'end', text=dates[i], values=treelist[i], iid=str(i))
+
+# textarea.insert(INSERT, "날짜", INSERT, " "*20, INSERT, "금액", INSERT, " "*20, INSERT, "카테고리", INSERT, " "*20, INSERT, "비고", INSERT, "\n")
+incomeBtn = Button(historyFrame, text = "+", font="나눔고딕 10", anchor="center", command = addList)
 incomeBtn.config(width = 5, height = 5)
-deleteBtn = Button(historyFrame, text = "삭제", font="나눔고딕 10")
+deleteBtn = Button(historyFrame, text = "삭제", font="나눔고딕 10", anchor="center")
 deleteBtn.config(width = 5, height = 5)
-textarea.grid(row=1, column=0, columnspan=4)
+treeview.grid(row=1, column=0, columnspan=4)
 deleteBtn.grid(row=2, column=2)
 incomeBtn.grid(row=2, column=3)
 
