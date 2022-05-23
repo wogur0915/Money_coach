ee8545f751751b145ca82427531b2fbc0bdaf005
Basilion <47267113+KorBasilion@users.noreply.github.com>
2022-05-23 18:45:03 +0900

    [Feat] 추가 경우의 수(elif) 기초 작업

--- function.py
+++ function.py
@@ -10,11 +10,15 @@ def show_frame(frame):
 def show_statistics_frame(frame, typeLogo, image):
     if typeLogoNum == 0:
         typeLogo.config(image=image, text="0")
+    elif typeLogoNum == 1:
+        typeLogo.config(image=image, text="1")
     frame.tkraise()
     
 def show_assets_frame(frame, commentsLogo, image):
     if commentsLogoNum == 0:
         commentsLogo.config(image=image, text="0")
+    elif commentsLogoNum == 1:
+        commentsLogo.config(image=image, text="1")
     frame.tkraise()
     
 # History Add Button Function
