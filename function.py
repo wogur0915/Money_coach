d27dbeeaced48d895560b6ff26186ef705d032a0
JH_T0mmy <34836246+wogur0915@users.noreply.github.com>
2022-05-26 08:47:31 +0900

    delList function 구현
    내역 내의 리스트 삭제 함수를 구현하였습니다.

--- function.py
+++ function.py
@@ -21,7 +21,8 @@ def show_assets_frame(frame, commentsLogo, image):
     elif commentsLogoNum == 1:
         commentsLogo.config(image=image, text="1")
     frame.tkraise()
-    
+
+# ------------------------------------------------------------------------------------------
 # History Add Button Function
 def addList(treeview) :
 
@@ -65,7 +66,7 @@ def addList(treeview) :
     addListWin = Tk()
     addListWin.title("가계부 추가")
     addListWin.geometry("300x350+942+190")
-    # addListWin.wm_attributes("-topmost", 1)  #창을 항상 맨위에 표시
+    # addListWin.wm_attributes("-topmost", 1)  # window on first
 
     # Enter Window
     dateLb = Label(addListWin, text="날짜", font="나눔고딕 13")
@@ -88,6 +89,76 @@ def addList(treeview) :
     tegType = Combobox(addListWin, width=17, height=10, values=types, justify = "center")
     inputOthers = Entry(addListWin, justify = "center")
     confirmBtn = Button(addListWin, text = "확인", command = incomeInputVal)
+
+    inputDate.grid(row=1, column=1)
+    inputExpOrInc.grid(row=3, column=1)
+    inputMoney.grid(row=5, column=1)
+    tegType.grid(row=7, column=1)
+    inputOthers.grid(row=9, column=1)
+    confirmBtn.grid(row=10, column=1, padx=10, pady=10)
+
+# ------------------------------------------------------------------------------------------
+# History Delete Button Function
+def delList(treeview) :
+    
+    # Return Enter Data(Global Value) to Apply History
+    def deleteInputVal() :
+        global dates, money, types, otherDetails, columns
+        value = [
+            inputDate.get(),
+            inputExpOrInc.get(),
+            inputMoney.get(),
+            tegType.get(),
+            inputOthers.get()
+            ]
+
+        isOK = True
+        for item in value[:4] :
+            # if empyty spaces exist
+            if not(item):
+                warning = messagebox.showwarning("경고!", "입력되지 않은 정보가 있습니다!")
+                isOK = False
+                break
+            # no empty then,
+        
+        if isOK:
+            if dates.index(value[0]) == expOrInc.index(value[1]) == money.index(value[2]) == types.index(value[3]) :
+                indexNum = dates.index(value[0])
+                treeview.delete(str(indexNum))
+
+            # Clean After Data Add
+            inputDate.delete(0,END)
+            inputExpOrInc.delete(0,END)
+            inputMoney.delete(0,END)
+            inputOthers.delete(0,END)
+            tegType.delete(0,END)
+
+    addListWin = Tk()
+    addListWin.title("가계부 삭제")
+    addListWin.geometry("300x350+942+190")
+    # addListWin.wm_attributes("-topmost", 1) # window on first
+
+    # Enter Window
+    dateLb = Label(addListWin, text="날짜", font="나눔고딕 13")
+    inoutLb = Label(addListWin, text="수입/지출", font="나눔고딕 13")
+    moneyLb = Label(addListWin, text="금액", font="나눔고딕 13")
+    tegLb = Label(addListWin, text="카테고리", font="나눔고딕 13")
+    memoLb = Label(addListWin, text="비고", font="나눔고딕 13")
+    dateLb.grid(row=0, column=1, padx=100, pady=7)
+    inoutLb.grid(row=2, column=1, padx=100, pady=7)
+    moneyLb.grid(row=4, column=1, padx=100, pady=7)
+    tegLb.grid(row=6, column=1, padx=100, pady=7)
+    memoLb.grid(row=8, column=1, padx=100, pady=7)
+
+    expOrIncTyp = ['수입', '지출']
+    expTypes = ['식비','주거/통신','의복/미용','건강/문화','교육/육아','교통/차량','경조사/회비','공과금','월급','기타']
+
+    inputDate = Entry(addListWin, justify = "center")
+    inputExpOrInc = Combobox(addListWin, width=17, height=10, values=expOrIncTyp, justify = "center")
+    inputMoney = Entry(addListWin, justify = "center")
+    tegType = Combobox(addListWin, width=17, height=10, values=expTypes, justify = "center")
+    inputOthers = Entry(addListWin, justify = "center")
+    confirmBtn = Button(addListWin, text = "확인", command = deleteInputVal)
 
     inputDate.grid(row=1, column=1)
     inputExpOrInc.grid(row=3, column=1)
