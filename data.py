c28b76de4e2736cc33a2e1ef499aa28ac7d974d3
JH_T0mmy <34836246+wogur0915@users.noreply.github.com>
2022-05-29 10:56:18 +0900

    지출의 합계와 수입의 합계, 수입에서 지출을 제외한 총 합계를 저장할 변수 데이터 추가
    지출의 합계와 수입의 합계, 수입에서 지출을 제외한 총 합계를 저장할 변수 데이터 추가 및 초기화 하였습니다.
    expSum, incSum, totalMoney 생성.

--- data.py
+++ data.py
@@ -5,7 +5,12 @@ types = []
 otherDetails = []
 expOrInc = []
 
-treelist=[]
+# Global Money Values
+expSum = 0
+incSum = 0
+totalMoney = 0
+
+# treeview columns
 columns = 0
 
 # Global Value For Statistics Page Logo
