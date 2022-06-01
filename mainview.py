e4eda0e62921a69acc9d22a24e77700ef9fe5fe1
Andrewendlesss <andrewendlesss@gmail.com>
2022-06-01 18:11:17 +0900

    Added 2 buttons and rearranged some things

--- mainview.py
+++ mainview.py
@@ -128,24 +128,29 @@ for frameName in (statisticsFrame, assetsFrame):
     
 # Setting page 
 
-design = Label(settingFrame, image=test, width=740, height=320, compound="c", bd=10, relief = RIDGE)
+design = Label(settingFrame, image=test, width=740, height=335, compound="c", bd=10, relief = RIDGE)
 design.place(x=20, y=60) 
    
 Help = Label(settingFrame, image=test, text="Help", font = ("Helvetica", 25), width=200, height=40, compound="c", activeforeground = "green")
 Help.place(x=310, y=115)
 
-Us = Label(settingFrame, image=test, text="About Us: We are CBNU students currently majoring in computer science. \nThis program is our first project that we made together. \n To contact us: andrewendlesss@gmail.com \n 희범의 email \n 재혁의 email \n License: MIT",font = ("Helvetica", 10), width=350, height=80, compound="c")
-Us.place(x=315, y=340) 
+Us = Label(settingFrame, image=test, text=" To contact the developers : \n 문희범: email@site.com \n 임재혁: email@site.com \n 최안드레이: email@site.com. \n Our License: MIT",font = ("나눔스퀘어", 10), width=170, height=80, compound="c")
+Us.place(x=600, y=420) 
 
-Version = Label(settingFrame, image=test, text= "BETA 0.1.0", width=190, height=30, compound="c", fg="light green", font=("Arial", 15), relief = RIDGE)
-Version.place(x=500, y=400) 
-
-Link = Button(settingFrame, text="Our GitHub repository", font=("Helvetica", 20),) 
+Link = Button(settingFrame, text="Our GitHub repository", font=("Helvetica", 15)) 
 Link.place(x=30, y=430)
 Link.bind("<Enter>", lambda e: label_hover(e, Link, settingFrame))
 Link.bind("<Leave>", lambda e: label_hover_leave(e, Link, settingFrame))
 Link.bind("<Button-1>", lambda e : callback(link))
 
+history = Label(settingFrame, image = test, text="History", font=("Helvetica", 23), width=80, height=25, compound="c",)
+history.place(x = 45, y = 75)
+statistics = Label(settingFrame, image= test, text="Statistics", font=("Helvetica", 23), width=100, height=40, compound="c",)
+statistics.place(x = 270, y = 70)
+assets = Label(settingFrame, image= test, text="Assets", font=("Helvetica", 23), width=80, height=40, compound="c",)
+assets.place(x = 470, y = 70)
+settings = Label(settingFrame, image= test, text="Settings", font=("Helvetica", 23), width=80, height=40, compound="c",)
+settings.place(x = 670, y = 70)
 # Main Roop & Set First Frame
 show_frame(lobbyFrame)
 main.mainloop()
