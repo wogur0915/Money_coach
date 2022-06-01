9f570af0e70eea9a7a7c77f04ebfc10096cc8c39
Andrewendlesss <andrewendlesss@gmail.com>
2022-06-01 21:09:36 +0900

    Added full information about history, statistics, and assets

--- data.py
+++ data.py
@@ -14,3 +14,6 @@ About = "We are CBNU students currently majoring in computer science. \n This pr
 license = "                "
 agreement = "Privacy Policy of House-hold Account Book System In order to receive information about your Personal Data, \nthe purposes and the parties the Data is shared with, contact the Owner. \n For more information and to understand your rights, you can also view the complete version of this privacy policy, \n by contacting us. Contact information Owner and Data Controller CBNU university \n Owner contact email: andrewendless@gmail.com \n Terms & Conditions: By using any program, you indicate that you have read and accept our Terms, \nincluding the applicable Additional Terms and agree to abide by\n and be bound by all such Terms(as modified from time to time).\n If you do not accept all applicable Terms,\n please refrain from using this software."
 review = "https://www.surveymonkey.com/r/33PK6GW"
+history = "- + 버튼을 사용해 내역을 추가할 수 있습니다. \n 삭제하고싶은 내역을 선택한 후, 버튼을 누르면 삭제하실 수 있습니다. \n입력한 내역을 저장하실 때에는 상단의 저장 버튼을 누른 후, 저장 위치를 선택하시면 저장이 가능합니다. \n 기존 데이터를 불러오실 때는 상단의 불러오기를 통해 데이터 파일을 선택하면 데이터를 불러오실 수 있습니다"
+statistics = "- 입력하신 소비 내역 중에서 가장 많은 금액을 사용한 상위 5개의 카테고리를 확인하실 수 있습니다.\n- 소비 성향은 가장 많은 소비 내역을 기준으로 소비 성향을 도출해주도록 되어있습니다."
+assets =  "입력하신 내역 데이터를 분석해 현재 남은 자산과 소비, 지출 총액을 보여드립니다. \n- 분석된 데이터를 토대로 소비에 대한 플래닝을 제시해드립니다."
