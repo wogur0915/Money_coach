e672e5360dffefbf4960e7667c791456b1c9b268
Andrewendlesss <andrewendlesss@gmail.com>
2022-06-02 23:27:24 +0900

    undid last commit

--- module.py
+++ module.py
@@ -4,7 +4,3 @@ from tkinter.ttk import Combobox
 from tkinter import messagebox
 from tkinter.filedialog import asksaveasfile, askopenfilename, SaveAs
 import csv
-import webbrowser
-import tkinter as tk
-from tkinter import ttk
-import time
