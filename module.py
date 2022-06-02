4e0afe92b4ad5f0e1a6ca62a6ec38d8f4af6c7e4
Andrewendlesss <andrewendlesss@gmail.com>
2022-06-02 22:31:13 +0900

    Merge

--- module.py
+++ module.py
@@ -4,3 +4,7 @@ from tkinter.ttk import Combobox
 from tkinter import messagebox
 from tkinter.filedialog import asksaveasfile, askopenfilename, SaveAs
 import csv
+import webbrowser
+import tkinter as tk
+from tkinter import ttk
+import time
