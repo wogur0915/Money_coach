df03c1d8d0407df2c4138fc628e591bbdf1e22a3
Basilion <47267113+KorBasilion@users.noreply.github.com>
2022-06-02 22:31:12 +0900

    [Feat] 주/월간 버튼에 따른 예외처리

--- frameswitch.py
+++ frameswitch.py
@@ -23,6 +23,10 @@ def show_assets_frame(frame, commentsLogo, assetsSumPrice, assetsPlusPrice, asse
         assetsPlusPrice.config(text=(str(sumIncomes())+" 원"))
         assetsMinusPrice.config(text=(str(sumExpends())+" 원"))
         assetsSumPrice.config(text=(str(total())+" 원"))
+    else:
+        assetsPlusPrice.config(text=("1 원"))
+        assetsMinusPrice.config(text=("1 원"))
+        assetsSumPrice.config(text=("1 원"))
     if commentsLogoNum == 0:
         commentsLogo.config(image=plan0)
     elif commentsLogoNum == 1:
