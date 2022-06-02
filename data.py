e71c42a6919c851b17fd67d5d0ea396a4e325389
Andrewendlesss <andrewendlesss@gmail.com>
2022-06-02 22:49:44 +0900

    added the changes

--- data.py
+++ data.py
@@ -5,8 +5,29 @@ types = []
 otherDetails = []
 expOrInc = []
 
-treelist=[]
-i = 0
+# Global Money Values
+expSum = 0
+incSum = 0
+totalMoney = 0
+
+# treeview columns
+columns = 0
+
+# For Design Test
+historySum = 0
+historyPlus = 1
+historyMinus = 2
+
+class catagory:
+    name = []
+    sum = 0
+rank = []
+
+# Global Value For Statistics Page Logo
+typeLogoNum = 0
+
+# Global Value For Assets Page Logo
+commentsLogoNum = 0
 
 # Setting data
 link = "https://github.com/KorBasilion/OSS-Basic-Project"
